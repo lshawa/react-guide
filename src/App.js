ea49fe57b19843fe989d9ae787d66ba015414442
Laraine Shawa <laraineshawa@Laraines-MacBook-Pro.local>
2019-01-03 09:59:47 -0600

    added the component to app.js to render on the home page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-
+        <Person/>
       </div>
   
     );
