9b3d699d481debab6b52efce4756e5dd28686f2c
Laraine Shawa <laraineshawa@Laraines-MacBook-Pro.local>
2019-01-03 11:31:38 -0600

    to add content into your coomponent from outside using children.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 //component is a function returning some jsx
 const person = (props) => { 
-    return <p>I am {props.name} and I am {props.age} years old!</p>
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old!</p>
+            <p> {props.children} </p>
+
+        </div>
+    )
 }
 
 export default person; 
