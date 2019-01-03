f685e8c59fc27d5a1614f30db91211898a51735f
Laraine Shawa <laraineshawa@Laraines-MacBook-Pro.local>
2019-01-03 13:28:45 -0600

    stop hardcoding name
    - Instead of hardcoding it, now I'm accessing this property in this object here in persons array.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
+
+  //Only available in class components 
+  state = {
+    persons: [
+      {name: 'Max', age: 28}, 
+      {name: 'Manu', age: 29}, 
+      {name: 'Steph', age: 26}
+    ]
+  }
   render() {
     return (
       <div className="App">
@@ -9,7 +18,7 @@ class App extends Component {
         <p>This is really working!</p>
         <button>Switch Name</button>
         <Person
-          name="Max"
+          name={this.state.persons[0].name}       //refers to the class
           age="28"
         />
         <Person name="Manu" age="29">
