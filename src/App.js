bb689f4bdfca778faf247f24dd7ebcb46c4c9e36
Laraine Shawa <laraineshawa@Laraines-MacBook-Pro.local>
2019-01-03 15:55:02 -0600

    change the name by clicking
    - this is done by passing an argument.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // this.state.persons[0].name = 'Maximillion'; DONT DO THIS
     this.setState({
       persons: [
-        {name: 'Maxamillion', age: 28}, 
+        {name: newName, age: 28}, 
         {name: 'Manu', age: 29}, 
         {name: 'Steph', age: 27}
       ]
@@ -28,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Maxamillion')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}       //refers to the class
           age={this.state.persons[0].age}
@@ -36,7 +36,7 @@ class App extends Component {
         <Person 
            name={this.state.persons[1].name} 
            age={this.state.persons[1].age}
-           click = {this.switchNameHandler} > My hobbies: Racing
+           click = {this.switchNameHandler.bind(this, 'MAX!')} > My hobbies: Racing
         </Person>
 
         <Person
