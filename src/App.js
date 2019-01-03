6559b70330e0d968659d04b7f25f960999644f96
Laraine Shawa <laraineshawa@Laraines-MacBook-Pro.local>
2019-01-03 11:44:17 -0600

    add button to switch name of the person dynamically

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
+        <button>Switch Name</button>
         <Person
-          name = "Max"
-          age = "28"
+          name="Max"
+          age="28"
         />
-        <Person name = "Manu" age = "29">
+        <Person name="Manu" age="29">
           My hobbies: Racing
         </Person>
-        
-      
+
         <Person
-          name = "Steph"
-          age = "26"
+          name="Steph"
+          age="26"
         />
 
       </div>
-  
+
     );
   }
 }
